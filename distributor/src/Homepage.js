969f192ee331156d33ef2d7a802cc1da4eef37e3
caesar <joaovictorcsatu@gmail.com>
2022-05-27 09:49:37 -0300

    Update to navbar

--- distributor/src/Homepage.js
+++ distributor/src/Homepage.js
@@ -1,5 +1,34 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
 const Body = () => {
-    return <></>
+    return (
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+            <div className="container-fluid">
+                <div>
+                    <a className="navbar-brand">Distributor</a>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#Home">
+                                    Home
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#Catalog">
+                                    Catalog
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#About">
+                                    About
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Body
