730fc87b0d97106cefe3f4bf98accd302f3ba190
caesar <joaovictorcsatu@gmail.com>
2022-05-29 11:49:13 -0300

    Added initial Modal draft

---
+++ distributor/src/Components/Modal/Modal.js
@@ -0,0 +1,55 @@
+const ModalPages = () => {
+    const [show, setShow] = useState(false)
+
+    const [page, setPage] = useState(1)
+
+    const handleClose = () => setShow(false)
+
+    const handleShow = () => setShow(true)
+
+    const nextPage = () => setPage(2)
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+            </Button>
+            {page == 1 ? (
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Woohoo, you're reading this text in a modal!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={nextPage}>
+                            Next
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            ) : (
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Woohoo, you're reading this text in a modal number 2!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Cancel
+                        </Button>
+                        <Button variant="primary" onClick={nextPage}>
+                            Submit
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            )}
+        </>
+    )
+}
+
+export default ModalPages
