009cd489cba3d327e8c1ac8a8bdf0a6a328b5bb4
caesar <joaovictorcsatu@gmail.com>
2022-05-28 23:11:40 -0300

    updated file structure and added carousel

---
+++ distributor/src/Assets/Data.js
@@ -0,0 +1,5 @@
+const Data = [
+    { id: 1, Title: 'Tile 1', Description: 'Description 1', Price: 'Price 1' },
+]
+
+export default Data
---
+++ distributor/src/Components/Carousel/Carousel.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import CarouselItem from './Carousel_item'
+import Data from '../../Assets/Data'
+
+const Carousel = () => {
+    const [data, setData] = useState(Data)
+
+    return (
+        <div
+            id="carouselExampleControls"
+            className="carousel slide"
+            data-bs-ride="carousel"
+        >
+            <div className="carousel-inner">
+                {data.map((x) => {
+                    const { id, Title, Description, Price } = x
+
+                    return (
+                        <div key={id} className="carousel-item">
+                            <h2>{Title}</h2>
+                            <p>{Description}</p>
+                            <div>{Price}</div>
+                        </div>
+                    )
+                })}
+            </div>
+            <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleControls"
+                data-bs-slide="prev"
+            >
+                <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleControls"
+                data-bs-slide="next"
+            >
+                <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    )
+}
+
+export default Carousel
