f23a48d595736f074791def54149e6b1ad299f3f
caesar <joaovictorcsatu@gmail.com>
2022-05-30 09:09:09 -0300

    Added both pages to modal component

--- distributor/src/Components/Modal/Modal.js
+++ distributor/src/Components/Modal/Modal.js
@@ -2,36 +2,54 @@
 import React, { useState } from 'react'
 import { Button, Container, Modal } from 'react-bootstrap'
 
-export const ContextProvider = React.createContext(null)
-
 const ModalPages = () => {
-    const [show, setShow] = useState(false)
+    const [show, setShow] = useState(null)
+    const [modalState, setModalState] = useState(null)
 
-    const handleClose = () => setShow(false)
-    const handleShow = () => setShow(true)
+    const handleClose = () => [setModalState(null), setShow(false)]
+    const handleShow = () => [setShow(true), setModalState(1)]
+    const nextPage = () => setModalState(2)
 
     return (
         <Container className="d-grid gap-2">
             <Button variant="primary" onClick={handleShow} size="lg">
                 Launch demo modal
             </Button>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Woohoo, you're reading this text in a modal!
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+            {modalState === 1 ? (
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Woohoo, you're reading this text in a modal!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={nextPage}>
+                            Next
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            ) : (
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading 2</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Woohoo, you're reading this text in a second modal!
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Submit
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            )}
         </Container>
     )
 }
