70bf0cc1ee0939a4725361a12603dd87c534d346
caesar <joaovictorcsatu@gmail.com>
2022-06-01 09:30:43 -0300

    Update to modal pages

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,14 +1,23 @@
 //TO DO - Add submit functionality with Axios
 import React, { useState } from 'react'
-import { Button, Container, Modal } from 'react-bootstrap'
+import { Button, Container, Modal, Form, Col, Row } from 'react-bootstrap'
 
-const ModalPages = () => {
+const ModalPages = (props) => {
     const [show, setShow] = useState(null)
     const [modalState, setModalState] = useState(null)
-
     const handleClose = () => [setModalState(null), setShow(false)]
     const handleShow = () => [setShow(true), setModalState(1)]
     const nextPage = () => setModalState(2)
+    const [state, setState] = useState({ unidadeTempo: 'Minutos' })
+
+    function handleInputChange(event) {
+        const target = event?.target
+        const value = target?.type == 'checkbox' ? target.checked : target.value
+        const name = target?.name
+
+        state[name] = value
+        setState(state)
+    }
 
     return (
         <Container className="d-grid gap-2">
@@ -16,12 +25,100 @@ const ModalPages = () => {
                 Launch add modal
             </Button>
             {modalState === 1 ? (
-                <Modal show={show} onHide={handleClose}>
+                <Modal show={show} onHide={handleClose} size="lg">
                     <Modal.Header closeButton>
                         <Modal.Title>Modal heading</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        Woohoo, you're reading this text in a modal!
+                        <Form>
+                            <Row>
+                                <Col md={5}>
+                                    <Form.Group>
+                                        <Form.Label>Nome da Receita</Form.Label>
+                                        <Form.Control
+                                            onChange={handleInputChange}
+                                            name="nomeReceita"
+                                            type="text"
+                                        />
+                                    </Form.Group>
+                                </Col>
+
+                                <Col md={2}>
+                                    <Form.Group>
+                                        <Form.Label>
+                                            Tempo de preparo
+                                        </Form.Label>
+                                        <Form.Control
+                                            onChange={handleInputChange}
+                                            name="tempoPreparo"
+                                            type="number"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                                <Col md={2}>
+                                    <Form.Group>
+                                        <Form.Label>
+                                            Unidade de tempo
+                                        </Form.Label>
+                                        <Form.Select
+                                            onChange={handleInputChange}
+                                            name="unidadeTempo"
+                                        >
+                                            <option>Minutos</option>
+                                            <option>Horas</option>
+                                        </Form.Select>
+                                    </Form.Group>
+                                </Col>
+                                <Col md={2}>
+                                    <Form.Group>
+                                        <Form.Label>Porções</Form.Label>
+                                        <Form.Control
+                                            name="porcoes"
+                                            type="text"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={5}>
+                                    <Form.Group>
+                                        <Form.Label>Categoria</Form.Label>
+                                        <Form.Control
+                                            name="categoria"
+                                            type="text"
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label>Modo de preparo</Form.Label>
+                                        <textarea
+                                            onChange={handleInputChange}
+                                            name="modoPreparo"
+                                            className="form-control"
+                                        ></textarea>
+                                    </Form.Group>
+                                </Col>
+                                <Col md={5}>
+                                    <Form.Group>
+                                        <Form.Label>
+                                            Adicione uma imagem
+                                        </Form.Label>
+                                        <input
+                                            name="imagem"
+                                            className="form-control"
+                                            type="file"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Button
+                                onClick={() => props.addReceitas(state)}
+                                variant="success"
+                                type="button"
+                            >
+                                Salvar
+                            </Button>
+                        </Form>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleClose}>
@@ -33,12 +130,41 @@ const ModalPages = () => {
                     </Modal.Footer>
                 </Modal>
             ) : (
-                <Modal show={show} onHide={handleClose}>
+                <Modal show={show} onHide={handleClose} size="lg">
                     <Modal.Header closeButton>
                         <Modal.Title>Modal heading 2</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        Woohoo, you're reading this text in a second modal!
+                        <Form.Group>
+                            <Row>
+                                <Col>
+                                    <Form.Label>Ingredientes</Form.Label>
+                                    <Form.Control
+                                        name="ingrediente"
+                                        type="text"
+                                        onChange={handleInputChange}
+                                    />
+                                </Col>
+                                <Col>
+                                    <Form.Group>
+                                        <Form.Label>Quantidade</Form.Label>
+                                        <Form.Control
+                                            name="quantidade"
+                                            type="number"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                                <Col>
+                                    <Form.Group>
+                                        <Form.Label>Unidade</Form.Label>
+                                        <Form.Control
+                                            name="unidade"
+                                            type="text"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                        </Form.Group>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleClose}>
